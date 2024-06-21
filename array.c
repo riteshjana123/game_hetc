8cc393c2a56bede307e33f26c1737e839fb167fc
riteshjana123 <riteshjana525@gmail.com>
2024-06-21 13:13:00 +0530

    add array file

---
+++ array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include<string.h>
+struct student
+{
+    int roll;
+    char name[50];
+    char branch[50];
+    float cgpa;
+    char grade;
+
+}s1,s2;
+
+int main(){
+
+s1.roll = 1;
+strcpy(s1.name,"Ayushman kundu");
+strcpy(s2.name,"Adarsh kumar sigh");
+
+
+return 1;
+}
